ce56b729913c07ffca09c94fbf7fcd3450939fb2
Henrique Ramos <chenrique.ramos12@gmail.com>
2020-08-11 18:52:22 -0300

    Fix Access-Control-Allow-Origin

--- src/index.js
+++ src/index.js
@@ -1,33 +1,37 @@
-const express = require('express');
-const morgan = require('morgan');
-const passport = require('passport');
-const session = require('express-session');
-var cookieParser = require('cookie-parser');
+const express = require("express");
+const morgan = require("morgan");
+const passport = require("passport");
+const session = require("express-session");
+var cookieParser = require("cookie-parser");
 
 const app = express();
 
+require("./plugins");
 
-require('./plugins');
+app.set("port", process.env.PORT || 5000);
 
-app.set('port', process.env.PORT || 5000);
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  next();
+});
 
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 
 app.use(express.json());
 
-app.use(cookieParser())
+app.use(cookieParser());
 
-app.use(session({
-    secret: 'secret',
+app.use(
+  session({
+    secret: "secret",
     resave: true,
-    saveUninitialized: true
-}));
+    saveUninitialized: true,
+  })
+);
 
 app.use(passport.initialize());
 app.use(passport.session());
 
+app.use("/api", require("./routes"));
 
-
-app.use('/api', require('./routes'));
-
-app.listen(app.get('port'));
+app.listen(app.get("port"));
